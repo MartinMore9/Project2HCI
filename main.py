2219e29e7133fec9f71c90bc51662022204b0a5a
Martin Moreano <martin.moreano@gmail.com>
2022-11-08 07:53:47 -0500

    Please no delete comments

--- main.py
+++ main.py
@@ -3,19 +3,84 @@
 
 import http.client
 import json
+import requests
+import time
+
 import streamlit as st
 import numpy as np
 import pandas as pd
 from collections import namedtuple
 from array import *
+###############################################################
+venueList = set()
+mapData = {
+    "lat": [],
+    "lon": []
+}
+lineChartData = {}
+dataFrameData = {
+    "Round": [],
+    "Home Team": [],
+    "Away Team": [],
+    "Score": [],
+    "Referee": [],
+    "Stadium": []
+}
 
-
-
+###############################################################
 class obj:
 
     # constructor
     def __init__(self, dict1):
         self.__dict__.update(dict1)
+
+################################################################
+
+# def updateDataFrame():
+#     dataFrameOBJ = st.empty()
+#     fillDataFrame()
+#     dataFrameOBJ= st.dataframe(dataFrameData)
+
+################################################################
+
+def fillMap():
+
+    for venue in venueList:
+
+        # tempstr = "/venues?name=" + str(venue)
+        # tempstr = tempstr.replace(" ", "%20")
+        # tempstr = tempstr.encode('ascii', 'ignore').decode('ascii')
+
+        conn.request("GET", "/venues?id=" + str(venue), headers=headers)
+
+        res = conn.getresponse()
+        data = res.read()
+
+        y = json.loads(data.decode("utf-8"))
+        venuesListObj = json.loads(json.dumps(y), object_hook=obj)
+
+        ################################################################################
+
+        name = str(venuesListObj.response[0].name).replace(" ","%20")
+        address = str(venuesListObj.response[0].address).replace(" ","%20")
+        city = str(venuesListObj.response[0].city).replace(" ", "%20")
+        country = str(venuesListObj.response[0].country).replace(" ", "%20")
+
+        url = "https://api.geoapify.com/v1/geocode/search?name=" + name +"&street=" + address +"&city="+ city +"&country="+ country +"&limit=1&format=json&apiKey=4136e494e7a14ee28368c1191cdac050"
+
+        response = requests.get(url)
+        venueData = response.json()
+        # print(response.json())
+        # temp = json.loads(response.json())
+        # venueData = json.loads(json.dumps(temp), object_hook=obj)
+        #print(venueData)
+        mapData["lat"].append(venueData["results"][0]["lat"])
+        mapData["lon"].append(venueData["results"][0]["lon"])
+
+    tempDf = pd.DataFrame.from_dict(mapData, orient= 'columns')
+    return tempDf
+
+###############################################################################
 
 conn = http.client.HTTPSConnection("v3.football.api-sports.io")
 
@@ -30,27 +95,138 @@ conn.request("GET", "/fixtures?league=2&season=2022", headers=headers)
 res = conn.getresponse()
 data = res.read()
 
-x = json.loads(data.decode("utf-8"))
-dataObj = json.loads(json.dumps(x), object_hook=obj)
+temp = json.loads(data.decode("utf-8"))
+fixtureData = json.loads(json.dumps(temp), object_hook=obj)
 
-array2 = []
+###############################################################################
 
-for match in dataObj.response:
 
-    temp = []
+###########################################################################
 
-    temp.append(str(match.teams.home.name) + " - " + str(match.teams.away.name))
-    temp.append(str(match.goals.home) + " - " + str(match.goals.away))
-    array2.append(temp)
+conn.request("GET", "/teams?league=2&season=2022", headers=headers)
 
-# print(dataObj.response[0].fixture.id)
-# print(array2)
+res = conn.getresponse()
+data = res.read()
 
-test = np.array(array2)
+y = json.loads(data.decode("utf-8"))
+teamsList = json.loads(json.dumps(y), object_hook=obj)
 
-df = pd.DataFrame(
-    test,
-    columns=(["Fixture", "Score"]))
+teamOptions = []
 
-st.dataframe(df)
-st.dataframe(df)
+for team in teamsList.response:
+    teamOptions.append(team.team.name)
+
+#options = st.multiselect('Select Team(s):', teamOptions, default=teamOptions)
+
+#############################################################################
+
+def fillDataFrame():
+    #filteredData = []
+
+    for match in fixtureData.response:
+
+        if match.fixture.venue.id is not None:
+
+            venueList.add(int(match.fixture.venue.id))
+
+        if str(match.fixture.status.short) == "FT":
+
+            dataFrameData["Round"].append(str(match.league.round))
+            dataFrameData["Home Team"].append(str(match.teams.home.name))
+            dataFrameData["Away Team"].append(str(match.teams.away.name))
+            dataFrameData["Score"].append(str(match.goals.home) + " - " + str(match.goals.away))
+            dataFrameData["Referee"].append(str(match.fixture.referee))
+            dataFrameData["Stadium"].append(str(match.fixture.venue.name))
+
+    tempDf = pd.DataFrame(dataFrameData)
+
+    return tempDf
+
+###############################################################################
+def fillLineChart():
+    #filteredData = []
+
+    for match in fixtureData.response:
+
+        if str(match.fixture.status.short) == "FT":
+
+            homeTeam = str(match.teams.home.name)
+            awayTeam = str(match.teams.away.name)
+            homeTeamGoals = 0 if match.goals.home is None else int(match.goals.home)
+            awayTeamGoals = 0 if match.goals.home is None else int(match.goals.away)
+
+            if homeTeam not in lineChartData:
+                lineChartData[homeTeam] = [homeTeamGoals]
+            else:
+                #lineChartData[homeTeam].append(homeTeamGoals + lineChartData[homeTeam][len(lineChartData[homeTeam]) - 1])
+                lineChartData[homeTeam].append(homeTeamGoals + (lineChartData[homeTeam][len(lineChartData[homeTeam]) - 1] * len(lineChartData[homeTeam])))
+                lineChartData[homeTeam][len(lineChartData[homeTeam]) - 1] /= len(lineChartData[homeTeam])
+
+
+            if awayTeam not in lineChartData:
+                lineChartData[awayTeam] = [awayTeamGoals]
+            else:
+                #lineChartData[awayTeam].append(awayTeamGoals + lineChartData[awayTeam][len(lineChartData[awayTeam]) - 1])
+                lineChartData[awayTeam].append(awayTeamGoals + (lineChartData[awayTeam][len(lineChartData[awayTeam]) - 1] * len(lineChartData[awayTeam])))
+                lineChartData[awayTeam][len(lineChartData[awayTeam]) - 1] /= len(lineChartData[awayTeam])
+
+    #print(lineChartData)
+    #tempDf = pd.DataFrame(lineChartData)
+    #tempDf = pd.DataFrame([lineChartData])
+    tempDf = pd.DataFrame.from_dict(lineChartData, orient= 'index')
+    tempDf = tempDf.transpose()
+    tempDf = tempDf.rename(index=lambda x: int(x+1))
+    #print(tempDf)
+    #tempDf = pd.DataFrame.from_dict(lineChartData, orient='index')
+
+    return tempDf
+
+###############################################################################
+
+def fillBarChart():
+
+    for match in fixtureData.response:
+
+        if match.fixture.venue.id is not None:
+
+            venueList.add(int(match.fixture.venue.id))
+
+        if str(match.fixture.status.short) == "FT":
+
+            dataFrameData["Round"].append(str(match.league.round))
+            dataFrameData["Home Team"].append(str(match.teams.home.name))
+            dataFrameData["Away Team"].append(str(match.teams.away.name))
+            dataFrameData["Score"].append(str(match.goals.home) + " - " + str(match.goals.away))
+            dataFrameData["Referee"].append(str(match.fixture.referee))
+            dataFrameData["Stadium"].append(str(match.fixture.venue.name))
+
+    tempDf = pd.DataFrame(dataFrameData)
+
+    return tempDf
+
+#############################################################################
+
+df = fillDataFrame()
+lc = fillLineChart()
+
+#############################################################################
+
+
+
+st.sidebar.header("Filter Here:")
+filteredTeams = st.sidebar.multiselect('Select Team(s):', options=teamOptions, default="Paris Saint Germain")
+
+df_selection = df.query("`Home Team` == @filteredTeams | `Away Team` == @filteredTeams")
+#lc_selection = lc.query(index = "@filteredTeams")
+#lc_selection = lc.loc[0].loc[filteredTeams]
+lc_selection = lc.loc[:,filteredTeams]
+#lc_selection = lc.loc[filteredTeams]
+#lc_selection = lc_selection.
+#print(lc_selection)
+#print(venueList)
+st.dataframe(df_selection)
+#st.dataframe(lc_selection)
+st.line_chart(lc_selection)
+mp = fillMap()
+st.map(mp)
+#print(mapData)
